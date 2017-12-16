3f913daa0f894b0aaa07429f41059d6b102aed2c
LEARNING CENTER <LEARNING CENTER@PC12LAB2>
2017-12-15 22:27:52 -0300

    Se actualizaron todas las carpetas.

--- src/modelo/ModeloAgregar.java
+++ src/modelo/ModeloAgregar.java
@@ -20,21 +20,24 @@ public class ModeloAgregar {
     public boolean agregarProducto (Producto producto) throws SQLException{
         ModeloBuscar modBuscar= new ModeloBuscar();
         ArrayList<Producto> lista = modBuscar.buscarProducto(producto.getCodigo());
+        boolean flag = false;
         if (lista.isEmpty()){
+            System.out.println("Insertar registro...");
             String consulta2 = "INSERT INTO PRODUCTO (NOMBRE_PRODUCTO, STOCK, PRECIO) VALUES ('(?)',(?), (?))";
-            PreparedStatement preparedStatement2 = acceso.prepareStatement(consulta2);
+            PreparedStatement preparedStatement2 = consulta.prepareStatement(consulta2);
             preparedStatement2.setString(1, producto.getNombreProducto());
             preparedStatement2.setInt(2, producto.getStock());
             preparedStatement2.setInt(2, producto.getPrecio());
             preparedStatement2.execute();
+            
+        } else {
+            flag =  
+            System.out.println("Insertar registro...");
         }
         boolean flag = false;
         System.out.println("Insertar registro...");
         Statement acceso;
-        if (respuesta==true){
-            flag = respuesta;
-        } else if (respuesta==false){
-	    flag = respuesta;          
+               
 	    
         } 
         return flag;
